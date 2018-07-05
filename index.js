b90666458c0ded1e117451119294f2b1955714a2
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-07-05 18:20:09 -0400

    add initial animation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log('working')
+
+const start = document.getElementById('start')
+console.log(start)
+
+var cssSelector = () =>
+  anime({
+    targets: '#cssSelector .el',
+    translateX: -250
+  })
+
+start.addEventListener('click', cssSelector)
+
+// create html for projects/about/contact
+// hide all off screen
+// when link clicked, animate onto screen
+// if current active section => simultaneously animate off screen
