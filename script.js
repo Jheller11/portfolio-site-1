6221361329779076b5117b522dda4381cdbe8894
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-09-18 10:55:40 -0400

    remove old html, css, js

--- script.js
+++
@@ -1,9 +0,0 @@
-let main = document.querySelector('main')
-let height = document.body.scrollHeight
-
-document.addEventListener('scroll', e => {
-  main.setAttribute(
-    'style',
-    `background-color: rgba(79, 90, 91, ${e.path[1].scrollY / height / 3}) `
-  )
-})
